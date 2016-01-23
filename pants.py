9f321ee503d250619fb1d0aa468dc67815eb9fdb
Simon Burton <simon@arrowtheory.com>
2016-01-24 07:42:47 +1100

    commit edits

--- pants.py
+++ pants.py
@@ -25,7 +25,9 @@ blue = rgb(0., 0., 0.8)
 lred = rgb(1., 0.4, 0.4)
 white = rgb(1., 1., 1.) 
 
-shade = rgb(0.75, 0.55, 0)
+shade0 = rgb(0.75, 0.55, 0)
+shade1 = rgb(0.70, 0.55, 0)
+shade2 = rgb(0.65, 0.50, 0)
 grey = rgb(0.75, 0.75, 0.75)
 
 light_shade = rgb(0.85, 0.65, 0.1)
@@ -45,6 +47,9 @@ center = [text.halign.boxcenter, text.valign.middle]
 
 st_dashed = [style.linestyle.dashed]
 st_dotted = [style.linestyle.dotted]
+
+st_thick = [style.linewidth.thick]
+st_Thick = [style.linewidth.Thick]
 
 st_tau = [style.linewidth.Thick, red, style.linecap.round]
 st_vac = [style.linewidth.thick, red]+st_dotted
@@ -184,7 +189,7 @@ def getgrad(x, y):
 
 def normalize(dv):
     x, y = dv
-    r = (x**2+y**2)**0.5
+    r = (x**2.0+y**2.0)**0.5
     return x/r, y/r
 
 def rotate(dv):
@@ -197,27 +202,49 @@ def getlevel(x, y):
     dv = rotate(dv)
     return dv
 
+def dot(d0, d1):
+    return d0[0]*d1[0] + d0[1]*d1[1]
 
-#print getv(5.5, 5.5), getv(5.6, 5.5)
-#print getv(5.5, 5.6), getv(5.6, 5.6)
-#print getgrad(5.5, 5.5)
 
-c = canvas.canvas()
-dx = 0.1
-dy = 0.1
+#def getline(x0, y0, x1, y1):
+def getline(*args):
 
-for i in range(H):
-  for j in range(W):
-    r = v[i, j]
-    cl = rgb(r, r, r)
-    #cl = color.gradient.RedBlue
-    c.fill(path.rect(j*dx, i*dy, 1.2*dx, 1.2*dy), [cl])
+    args1 = []
+    for arg in args:
+        if isinstance(arg, (list, tuple)):
+            args1 += list(arg)
+        else:
+            args1.append(arg)
+    assert len(args1)==4, repr( args1)
+    x0, y0, x1, y1 = args1
 
-def plotlevelset(x, y, length=100):
-    ps = [path.moveto(x*dx, y*dy)]
-    #while 1:
+    dv = x1-x0, y1-y0
+    #r = (dv[0]**2 + dv[1]**2)**0.5
+    #n = int(round(r))
+    n = 100
+    r = 1.*n
+    dv = dv[0]/r, dv[1]/r    
+
+    x, y = x0, y0
+    z = getv(x, y)
+    pts = [(x, y, z)]
+    for i in range(n):
+        x += dv[0]
+        y += dv[1]
+        pts.append((x, y, getv(x, y)))
+    return pts
+
+
+def getlevelset(x, y, length=None):
     z0 = getv(x, y)
-    for i in range(length):
+    pts = [(x, y, z0)]
+    dv0 = None
+
+    state = True
+
+    i = 0
+    while 1:
+        i += 1
 
         dv = getgrad(x, y)
         #print x, y, dv
@@ -225,8 +252,19 @@ def plotlevelset(x, y, length=100):
             break
 
         dv = rotate(dv)
-
         dv = normalize(dv)
+
+        if dv0 is None:
+            dv0 = dv
+        r = dot(dv, dv0)
+        if state and r<-0.9:
+            yield pts
+            pts = [pts[-1]]
+            state = 0
+        elif not state and r>0.95:
+            yield pts
+            break
+
         x += dv[0]
         y += dv[1]
     
@@ -239,21 +277,120 @@ def plotlevelset(x, y, length=100):
         x += dv[0]
         y += dv[1]
 
-        ps.append(path.lineto(x*dx, y*dy))
-    c.stroke(path.path(*ps), [white])
+        pts.append((x, y, z0))
+
+        if length and i>length:
+            break
+
+    #return pts
 
 
-rnd = lambda : 0.9*random() + 0.05
-for count in range(5):
-    #x, y = 0.2*W, 5.
-    x, y = rnd()*W, rnd()*H
-    #plotlevelset(x, y)
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#   pick a coordinate system:
+#
 
-plotlevelset(0.05*W, H/2., 200)
-plotlevelset(0.10*W, H/2., 150)
-plotlevelset(0.15*W, H/2., 50)
-plotlevelset(0.32*W, H/2., 50)
-plotlevelset(0.70*W, H/2., 50)
+c = canvas.canvas()
+dx = 0.1
+dy = 0.1
+
+def transform(x, y, z):
+    x1 = x + 0.3*y
+    y1 = 0.5*y + 4*H*z
+
+    return x1*dx, y1*dy
+
+#def transform(x, y, z):
+#    return x*dx, y*dy
+
+
+for y in range(H-2,0,-1):
+  for x in range(W-1):
+    z00 = v[y, x]
+    z01 = v[y+1, x]
+    z10 = v[y, x+1]
+    z11 = v[y+1, x+1]
+    if max(z00,z01,z10,z11)>0.5:
+        continue
+    r = 2*z00
+    cl = rgb(r,r,r)
+    #c.fill(path.rect(x*dx, y*dy, 1.2*dx, 1.2*dy), [cl])
+
+    ps = [
+        path.moveto(*transform(x, y, z00)),
+        path.lineto(*transform(x+1.2, y, z10)),
+        path.lineto(*transform(x+1.2, y+1.2, z11)),
+        path.lineto(*transform(x, y+1.2, z01)),
+        path.closepath()]
+
+    #c.fill(path.path(*ps), [cl])
+
+
+def plotpts(pts, deco, closed=False, fill=False):
+    x, y, z = pts[0]
+    ps = [path.moveto(*transform(x, y, z))]
+    for x, y, z in pts[1:]:
+        ps.append(path.lineto(*transform(x, y, z)))
+    if closed:
+        ps.append(path.closepath())
+    if fill:
+        c.fill(path.path(*ps), deco)
+    else:
+        c.stroke(path.path(*ps), deco)
+
+
+back, front = 0, 1
+
+coords = [0.05*W, 0.10*W, 0.15*W, 0.32*W, 0.70*W]
+levelsets = [list(getlevelset(coord, H/2.)) for coord in coords]
+
+rev = lambda items : list(reversed(items))
+
+edge = (
+    getline(coords[0], H/2., coords[1], H/2.) +\
+    rev(levelsets[1][front]) +\
+    getline(levelsets[1][front][0][:2], coords[4], H/2.) +\
+    rev(levelsets[4][front]) +\
+    getline(levelsets[4][front][0][:2], 
+        levelsets[0][front][0][:2]) +\
+    levelsets[0][front])
+
+plotpts(edge, [shade0]+st_Thick, closed=True, fill=True)
+
+edge = (
+    getline(coords[1], H/2., coords[2], H/2.) +\
+    rev(levelsets[2][front]) +\
+    getline(levelsets[2][front][0][:2], coords[3], H/2.) +\
+    rev(levelsets[3][front]) +\
+    getline(levelsets[3][front][0][:2], 
+        levelsets[1][front][0][:2]) +\
+    levelsets[1][front])
+
+plotpts(edge, [shade0]+st_Thick, closed=True, fill=True)
+
+back, front = getlevelset(coords[0], H/2.)
+plotpts(back, [black]+st_dashed)
+plotpts(front, [black]+st_thick)
+
+#pts = getline(coords[0], H/2., coords[1], H/2.)
+#plotpts(pts, [black]+st_thick)
+
+back, front = getlevelset(coords[1], H/2.)
+plotpts(back, [black]+st_dashed)
+plotpts(front, [black]+st_thick)
+
+back, front = getlevelset(coords[2], H/2.)
+plotpts(back+front, [shade2]+st_thick, closed=True, fill=True)
+plotpts(back+front, [black]+st_thick, closed=True)
+
+back, front = getlevelset(coords[3], H/2.)
+plotpts(back+front, [shade2]+st_thick, closed=True, fill=True)
+plotpts(back+front, [black]+st_thick, closed=True)
+
+back, front = getlevelset(coords[4], H/2.)
+plotpts(back+front, [shade2]+st_thick, closed=True, fill=True)
+plotpts(back+front, [black]+st_thick, closed=True)
+
+
 
 c.writePDFfile("pic-pants.pdf")
 
